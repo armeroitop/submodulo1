8050e9a0e1306b849bca09b57775772a247ab92d
David G <armeroitop@gmail.com>
2024-07-03 20:43:01 +0200

    ni idea del confli

--- alterRuta1/sub1.php
+++ alterRuta1/sub1.php
@@ -2,9 +2,10 @@
 $algo = 'texto del submodulo1';
 $texCom2 = 'texto añadido en commit 2';
 
-<<<<<<< HEAD
+
 $texExplota = 'texto conflicto';
-=======
+
+
 $textReserva = 'texto añadido en la rama reserva';
 $otro = 'meto mas texto';
->>>>>>> reserva
+
